6962bcbd2083220e0a04abbb5b40975dbbd05323
Michelly Hiratsuka Zorzi <michelly.hiratsuka@gmail.com>
2020-06-24 23:38:16 -0300

    Aula 1 - versão1 - ImersãoGame
    Primeira versão da aula 1 da Imersão Game Dev da Alura. Movimentação de cenário, exibir personagem na tela.

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let imgfundo;
+let imgpersonagem;
+
+let cenario;
+
+function preload(){
+  imgfundo = loadImage ('imagens/background/fundo.png');
+  imgpersonagem = loadImage ('imagens/personagem/Run0.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario (imgfundo, 3);
+}
+
+function draw() {
+  cenario.exibe();
+  cenario.move();
+  image(imgpersonagem, 0, height -135, 100, 125, 0, 0, 376, 520);
+}
+
+class Cenario {
+  constructor(imagem, velocidade){
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
+  }
+  exibe(){
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2, 0, width, height);
+  }
+  move(){
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+    
+    if(this.x1 < -width){
+       this.x1 = width;
+    }
+    if(this.x2 < -width){
+       this.x2 = width;
+    }
+  }
+}
